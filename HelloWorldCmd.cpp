294f09344907084cb300216ad53ac4381e8318aa
Erik Olsson <eriol726@student.liu.se>
2016-12-22 12:19:17 +0100

    removed Hello world filse

--- HelloWorldCmd.cpp
+++
@@ -1,9 +0,0 @@
-#include "HelloWorldCmd.h"
-
-void* HelloWorld::creator() { return new HelloWorld; }
-
-MStatus HelloWorld::doIt(const MArgList& argList) {
-	MGlobal::displayInfo("Hello World!");
-
-	return MS::kSuccess;
-}
